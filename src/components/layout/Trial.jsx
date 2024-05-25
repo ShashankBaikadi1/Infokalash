efe76ac6698f01d5df3497949310349b25044c46
shashank-baikadi <shashankbaikadi1@gmail.com>
2024-05-25 13:27:54 +0530

    refactor: Update Trial component layout and styles

--- src/components/layout/Trial.jsx
+++ src/components/layout/Trial.jsx
@@ -1,22 +1,25 @@
 import React from 'react'
+import { Box, Flex, Image, Text } from '@chakra-ui/react'
 import image1 from '../../assets/undraw_photos/undraw_data_reports_706v.png'
 import image2 from '../../assets/undraw_photos/undraw_data_reports_706v.png'
+
 const Trial = () => {
   return (
-    <div className='flex overflow-x-hidden'>
-      <div className='p-12 bg-red-200 w-[105vh]'>
-        <div className='flex flex-col justify-center items-center  bg-green-200 h-[50vh] w-[55vh] rounded-md'>
-          <img src={image1} alt="Description" className='h-[30vh] mb-10 rounded-lg' />
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </div>
-      </div>
-      <div className='p-12 bg-red-200 w-[105vh]'>
-        <div className='flex flex-col justify-center items-center  bg-green-200 h-[50vh] w-[55vh] rounded-md'>
-          <img src={image1} alt="Description" className='h-[30vh] mb-10 rounded-lg' />
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </div>
-      </div>
-    </div>
+    <Flex direction={{ base: 'column', md: 'row' }} overflowX='hidden'>
+      <Box p='12' bg='red.200' w={{ base: 'full', md: '105vh' }}>
+        <Flex direction='column' justify='center' align='center' bg='green.200' h='50vh' w='55vh' rounded='md'>
+          <Image src={image1} alt="Description" h='30vh' mb='10' rounded='lg' />
+          <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Text>
+        </Flex>
+      </Box>
+
+      <Box p='12' bg='red.200' w={{ base: 'full', md: '105vh' }}>
+        <Flex direction='column' justify='center' align='center' bg='green.200' h='50vh' w='55vh' rounded='md'>
+          <Image src={image2} alt="Description" h='30vh' mb='10' rounded='lg' />
+          <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Text>
+        </Flex>
+      </Box>
+    </Flex>
   )
 }
 
