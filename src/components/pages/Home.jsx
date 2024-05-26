7dd2358705ace87daa0dc9a5748d30d32b1ef288
shashank-baikadi <shashankbaikadi1@gmail.com>
2024-05-26 21:20:19 +0530

    refactor: Update Trial component layout and styles

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,3 +1,33 @@
+// import CallToAction from '../layout/CallToAction'
+// import Client_Carousel from '../layout/Client_Carousel'
+// import Features_V2 from '../layout/Features_V2'
+// import Footer from '../layout/Footer'
+// import Header from '../layout/Header'
+// import IndustryTabs from '../layout/Tab'
+// import ValuesSection from '../../components/layout/ValuesSection'
+// import Hero_Section_V3 from '../layout/Hero_Section_V3'
+// import CarouselComponent from '../layout/CarouselComponent'
+// import Trial from '../layout/Trial'
+
+// const Home = () => {
+//   return (
+//     <div className='container mx-auto'>
+//       <Header/>
+//       <Hero_Section_V3/>
+//       <ValuesSection/>
+//       <Features_V2/>
+//       <CarouselComponent />
+//       <IndustryTabs/>
+//       <Client_Carousel/>
+//       <CallToAction/>
+//       <Footer/>
+    
+//     </div>
+//   )
+// }
+
+// export default Home
+
 import CallToAction from '../layout/CallToAction'
 import Client_Carousel from '../layout/Client_Carousel'
 import Features_V2 from '../layout/Features_V2'
@@ -11,7 +41,7 @@ import Trial from '../layout/Trial'
 
 const Home = () => {
   return (
-    <div className='container mx-auto'>
+    <div className='container mx-auto flex flex-col items-center justify-center'>
       <Header/>
       <Hero_Section_V3/>
       <ValuesSection/>
@@ -21,9 +51,6 @@ const Home = () => {
       <Client_Carousel/>
       <CallToAction/>
       <Footer/>
-      {/* <div className='sm:'>
-        <Trial/>
-      </div> */}
     </div>
   )
 }
